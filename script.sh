0f0600ce0e35fafea544d99058ddae7b5f17b359
Roy Gal <roy.gal@biodata.com>
2025-06-12 14:13:39 +0300

    WIP

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [[ "$INPUT_MODE" == "diff" ]]; then
-  bash "$GITHUB_ACTION_PATH/ci/run_diff.sh" "$MERGE_BASE_REF" "$MERGE_BASE_SHA" "$CHANGED_RUBY_FILES"
+  bash "$GITHUB_ACTION_PATH/run_diff.sh" "$MERGE_BASE_REF" "$MERGE_BASE_SHA" "$CHANGED_RUBY_FILES"
 else
-  bash "$GITHUB_ACTION_PATH/ci/run_full.sh"
+  bash "$GITHUB_ACTION_PATH/run_full.sh"
 fi
